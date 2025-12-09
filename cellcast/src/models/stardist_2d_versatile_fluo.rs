356a04ceb94b2a1633beaaa0a2389b8a8b782fb3
Edward Evans <elevans2@wisc.edu>
2025-12-09 15:43:09 -0600

    Add prob, dist, and points collection and filtering

--- cellcast/src/models/stardist_2d_versatile_fluo.rs
+++ cellcast/src/models/stardist_2d_versatile_fluo.rs
@@ -4,7 +4,7 @@ use imgal::image::percentile_normalize;
 use imgal::threshold::manual_mask;
 use imgal::traits::numeric::AsNumeric;
 use imgal::transform::pad::reflect_pad;
-use ndarray::{Array1, Array2, Array3, ArrayBase, AsArray, Ix2, Ix3, ViewRepr, s};
+use ndarray::{Array2, Array3, ArrayBase, AsArray, Axis, Ix2, ViewRepr};
 
 use crate::networks::stardist::versatile_fluo_2d::Model;
 use crate::utils::{axes, border};
@@ -42,14 +42,14 @@ where
 {
     // create a view of the data
     let view: ArrayBase<ViewRepr<&'a T>, Ix2> = data.into();
+    let view_shape = view.shape();
 
     // set optional parameters if needed
     let pmin = pmin.unwrap_or(1.0);
     let pmax = pmax.unwrap_or(99.8);
 
-    // percentile normalize the input data and ensure that each axis of the
-    // input image into the network is divisiable by DIV factor, if not reflect
-    // pad the image to the computed dimensions
+    // percentile normalize the input data and reflect pad each axis to a size
+    // that is divisiable by DIV
     let norm = percentile_normalize(&view, pmin, pmax, None, None);
     let norm = norm.mapv(|v| v as f32);
     let pad_config: Vec<usize> = view
@@ -60,15 +60,14 @@ where
     let norm_pad = reflect_pad(&norm, &pad_config, Some(0)).unwrap();
     let pad_shape = norm_pad.shape().to_vec();
 
-    // create a 1-D tensor, the stardist network reshapes the 1D intput
-    // TODO: move or avoid the need for 1D flattening for speed up
-    // initialize an instance of the StarDist network
+    // initialize an instance of the StarDist network and reshape the data into
+    // a 1D tensor
     let device = Default::default();
     let stardist_model = Model::<Backend>::default();
     let tensor =
         Tensor::<Backend, 1>::from_floats(norm_pad.into_flat().as_slice().unwrap(), &device);
 
-    // predict object probabilites and radial distances
+    // run StarDist network prediction, returns object probabilites and radial distances
     let (prob, dist) = stardist_model.forward(tensor, (pad_shape[0] as i32, pad_shape[1] as i32));
     let prob: Vec<f32> = prob.into_data().into_vec().unwrap();
     let dist: Vec<f32> = dist.into_data().into_vec().unwrap();
@@ -78,37 +77,61 @@ where
         .expect("StarDist 2D object probabilites reshape failed.");
     let dist_arr = Array3::from_shape_vec((row, col, N_RAYS), dist)
         .expect("StarDist 2D radial distances reshape failed.");
-    let dist_arr = dist_arr.into_dimensionality::<Ix3>().unwrap();
 
-    // post-processing
+    // === post-processing ===
     // ensure all values in ray distances are at least 1e-3, prevents negative
     // or zero distances
     let dist_arr = dist_arr.mapv(|v| v.max(1e-3));
-    // TODO: implement the optimal NMS prob threshold functions, until then
+    // TODO: implement the optimal NMS prob threshold functions, until then use
     // this value is from StarDist2D for the "blobs.tif" sample data
-    let valid_mask = manual_mask(&prob_arr, 0.479071463157368);
-    let mut valid_mask = valid_mask.into_dimensionality::<Ix2>().unwrap();
-    border::clip_mask_border(&mut valid_mask.view_mut().into_dyn(), 2);
-    // TODO: consider parallelize this or stay sequential?
-    // collect a Vec<(usize, usize)> as valid inds coords and only visit those
+    // create a valid object mask and clip the board by 2 pixels
+    let mut valid_mask = manual_mask(&prob_arr, 0.479071463157368);
+    border::clip_mask_border(&mut valid_mask.view_mut(), 2);
+    let valid_mask = valid_mask.into_dimensionality::<Ix2>().unwrap();
+    // collect a Vec<(usize, usize)> as valid indices coords and only visit those
     // this will save lots of time even with bounds checking. We only need to visit
     // every pixel in the valid mask array once.
-    let valid_indices: Vec<(usize, usize)> = valid_mask
+    // collect all valid (row, col) positions to avoid iterating the mask repeatedly
+    let valid_pos: Vec<(usize, usize)> = valid_mask
         .indexed_iter()
         .filter(|&((_, _), &v)| v)
         .map(|((r, c), _)| (r, c))
         .collect();
-    let valid_prob: Vec<f32> = valid_indices
-        .iter()
-        .map(|&(r, c)| prob_arr[[r, c]])
-        .collect();
-    let valid_prob = Array1::from(valid_prob);
-    let mut valid_dist = Array2::<f32>::zeros((valid_indices.len(), N_RAYS));
+    // filter probabilities and distances with valid indices
+    let mut valid_prob: Vec<f32> = valid_pos.iter().map(|&(r, c)| prob_arr[[r, c]]).collect();
+    let mut valid_dist = Array2::<f32>::zeros((valid_pos.len(), N_RAYS));
     (0..N_RAYS).for_each(|n| {
-        valid_indices.iter().enumerate().for_each(|(i, &(r, c))| {
+        valid_pos.iter().enumerate().for_each(|(i, &(r, c))| {
             valid_dist[[i, n]] = dist_arr[[r, c, n]];
         });
     });
+    // scale each valid position by 2 and collect the indices of positions outside
+    // of the original image dimensions (used for point filtering)
+    let mut valid_pos: Vec<(usize, usize)> =
+        valid_pos.iter().map(|&(r, c)| (r * 2, c * 2)).collect();
+    let invalid_inds: Vec<usize> = valid_pos
+        .iter()
+        .filter(|&&(r, c)| r > view_shape[0] || c > view_shape[1])
+        .enumerate()
+        .map(|(i, &(_, _))| i)
+        .collect();
+    // filter positions, probabilities, and distances if there are invalid indices
+    if !invalid_inds.is_empty() {
+        valid_pos = valid_pos
+            .iter()
+            .enumerate()
+            .filter(|&(i, &(_, _))| !invalid_inds.contains(&i))
+            .map(|(_, &(r, c))| (r, c))
+            .collect();
+        valid_prob = valid_prob
+            .iter()
+            .enumerate()
+            .filter(|&(i, &_)| !invalid_inds.contains(&i))
+            .map(|(_, &v)| v)
+            .collect();
+        let valid_inds: Vec<usize> = valid_pos.iter().enumerate().map(|(i, &(_, _))| i).collect();
+        valid_dist = valid_dist.select(Axis(0), &valid_inds);
+    }
 
     (prob_arr, dist_arr)
 }
