706fa46248c6b9923628b4ca032a4463b396b06f
Edward Evans <elevans2@wisc.edu>
2025-12-10 14:08:28 -0600

    WIP port special polygon NMS from stardist
    I'm doing this by hand even with Claude helping me
    understand the nuances of C++ code that I'm too
    stupid to understand.

--- cellcast/src/lib.rs
+++ cellcast/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod models;
 pub mod networks;
+pub mod process;
+pub mod spatial;
 pub mod utils;
---
+++ cellcast/src/process/nms.rs
@@ -0,0 +1,10 @@
+use ndarray::{ArrayView1, ArrayView2};
+
+pub fn sparse_polygon_nms_2d(
+    polygon_dist: ArrayView2<f32>,
+    polygon_prob: ArrayView1<f32>,
+    polygon_pos: ArrayView2<usize>,
+    threshold: f32,
+) {
+    todo!("Implement sparse polygon Non Maximum supression 2D!");
+}
