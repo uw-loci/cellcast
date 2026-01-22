459e8a72fc468211585cbb559b241657259242cd
Edward Evans <elevans2@wisc.edu>
2026-01-22 14:00:18 -0600

    Add documentation to fetch functions

--- cellcast/src/utils/fetch.rs
+++ cellcast/src/utils/fetch.rs
@@ -9,7 +9,25 @@ use reqwest::blocking;
 
 const CACHE_NAME: &str = ".cache/cellcast/weights";
 
-/// TODO
+/// Fetch the requested weights.
+///
+/// # Description
+///
+/// Fetches the requested `.bin` weight file from the given `url`. If the
+/// weights have already been downloaded and saved in the cache directory then
+/// no download occurs and the cached weights are used.
+///
+/// # Arguments
+///
+/// * `url`: The URL to the `.bin` model weights.
+/// * `file_name`: The name of the model weights file.
+/// * `verbose`: If `true` then "INFO" status updates are printed to the
+///    console. If `false`, then nothing is printed.
+///
+/// # Returns
+///
+/// * `Ok(PathBuf)`: The validate path to the `.bin` weights file.
+/// * `Err(Error)`: If the `url` is not valid. If the `file_name` is not valid.
 pub fn fetch_weights(url: &str, file_name: &str, verbose: bool) -> Result<PathBuf, Box<dyn Error>> {
     let cache_dir = get_cache_dir()?;
     let weights_path = cache_dir.join(file_name);
@@ -27,7 +45,15 @@ pub fn fetch_weights(url: &str, file_name: &str, verbose: bool) -> Result<PathBu
     Ok(weights_path)
 }
 
-/// Download a file
+/// Download and save a .bin weights file.
+///
+/// # Arguments
+///
+/// * `url`: The URL to the `.bin` model weights.
+/// * `file_path`: The file path to the cache directory where the weights are to
+///   be saved.
+/// * `verbose`: If `true` then "INFO" status updates are printed to the
+///    console. If `false`, then nothing is printed.
 fn download_weights(url: &str, file_path: &PathBuf, verbose: bool) -> Result<(), Box<dyn Error>> {
     if verbose {
         println!("[INFO] Downloading weights from: {}", url);
@@ -40,7 +66,7 @@ fn download_weights(url: &str, file_path: &PathBuf, verbose: bool) -> Result<(),
     Ok(())
 }
 
-/// Get temporary cache directory for the system
+/// Get the weights cache directory.
 fn get_cache_dir() -> io::Result<PathBuf> {
     let dir = env::home_dir().unwrap_or_else(|| env::temp_dir());
     let dir = dir.join(CACHE_NAME);
