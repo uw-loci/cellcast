8d62100a55ee4a58778f5e130b5ef6b0e05c6511
Edward Evans <elevans2@wisc.edu>
2026-02-09 16:16:20 -0600

    Add documentation to lib.rs

--- cellcast/src/lib.rs
+++ cellcast/src/lib.rs
@@ -1,3 +1,10 @@
+//! The `cellcast` crate is a recast of cell segmentation models built on the Burn
+//! tensor and deep learning framework. This library aims to make access to cell
+//! segmentation models easier and hardware agnostic.
+//!
+//! ## Crate Status
+//!
+//! This crate is still under active development and it's API is not stable.
 pub mod geometry;
 pub mod labeling;
 pub mod models;
