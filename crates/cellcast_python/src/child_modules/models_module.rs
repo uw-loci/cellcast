ceb8f479af375ac0276e85b2e028c179572ea887
Edward Evans <elevans2@wisc.edu>
2026-06-24 10:20:37 -0500

    Add StarDist3D Python binding

--- crates/cellcast_python/src/child_modules/models_module.rs
+++ crates/cellcast_python/src/child_modules/models_module.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
-use crate::classes::stardist_classes::PyStarDist2D;
+use crate::classes::stardist_classes::{PyStarDist2D, PyStarDist3D};
 use crate::utils::py_import_module;
 
 /// Registration function for the "models" module and their submodules.
@@ -8,6 +8,8 @@ pub fn register_models_module(parent_module: &Bound<'_, PyModule>) -> PyResult<(
     let models_module = PyModule::new(parent_module.py(), "models")?;
     py_import_module("models");
     py_import_module("models.StarDist2D");
+    py_import_module("models.StarDist3D");
     models_module.add_class::<PyStarDist2D>()?;
+    models_module.add_class::<PyStarDist3D>()?;
     parent_module.add_submodule(&models_module)
 }
--- crates/cellcast_python/src/classes/stardist_classes.rs
+++ crates/cellcast_python/src/classes/stardist_classes.rs
@@ -146,3 +146,59 @@ impl PyStarDist2D {
         }
     }
 }
+
+#[pyclass(name = "StarDist3D")]
+pub struct PyStarDist3D(StarDist3D);
+
+#[pymethods]
+impl PyStarDist3D {
+    /// TODO
+    #[staticmethod]
+    #[pyo3(signature = (weights_path=None, gpu=None))]
+    pub fn init_fluo(weights_path: Option<&str>, gpu: Option<bool>) -> Self {
+        Self(StarDist3D::init_fluo(weights_path, gpu.unwrap_or(true)))
+    }
+    /// TODO
+    #[pyo3(signature = (data, pmin=None, pmax=None, prob_threshold=None, nms_threshold=None, axis=None))]
+    pub fn predict_fluo<'py>(
+        &self,
+        py: Python<'py>,
+        data: Bound<'py, PyAny>,
+        pmin: Option<f64>,
+        pmax: Option<f64>,
+        prob_threshold: Option<f64>,
+        nms_threshold: Option<f64>,
+        axis: Option<usize>,
+    ) -> PyResult<Bound<'py, PyArray3<u64>>> {
+        if let Ok(arr) = data.extract::<PyReadonlyArray3<u8>>() {
+            self.0
+                .predict_fluo(arr.as_array(), pmin, pmax, prob_threshold, nms_threshold, axis)
+                .map(|output| output.into_pyarray(py))
+                .map_err(imgal_error_to_pyerr)
+        } else if let Ok(arr) = data.extract::<PyReadonlyArray3<u16>>() {
+            self.0
+                .predict_fluo(arr.as_array(), pmin, pmax, prob_threshold, nms_threshold, axis)
+                .map(|output| output.into_pyarray(py))
+                .map_err(imgal_error_to_pyerr)
+        } else if let Ok(arr) = data.extract::<PyReadonlyArray3<u64>>() {
+            self.0
+                .predict_fluo(arr.as_array(), pmin, pmax, prob_threshold, nms_threshold, axis)
+                .map(|output| output.into_pyarray(py))
+                .map_err(imgal_error_to_pyerr)
+        } else if let Ok(arr) = data.extract::<PyReadonlyArray3<f32>>() {
+            self.0
+                .predict_fluo(arr.as_array(), pmin, pmax, prob_threshold, nms_threshold, axis)
+                .map(|output| output.into_pyarray(py))
+                .map_err(imgal_error_to_pyerr)
+        } else if let Ok(arr) = data.extract::<PyReadonlyArray3<f64>>() {
+            self.0
+                .predict_fluo(arr.as_array(), pmin, pmax, prob_threshold, nms_threshold, axis)
+                .map(|output| output.into_pyarray(py))
+                .map_err(imgal_error_to_pyerr)
+        } else {
+            Err(PyErr::new::<PyTypeError, _>(
+                "Unsupported array dtype, supported array dtypes are u8, u16, u64, f32, and f64.",
+            ))
+        }
+    }
+}
